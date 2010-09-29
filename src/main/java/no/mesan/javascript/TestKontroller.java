47fbde70a39051cff37cd08a55026405d08fa518
Christian Ihle <christiani@mesan.no>
2010-09-29 13:50:24 +0000

    Fungerende testkontroller og jsp
    git-svn-id: https://dev.mesan.no/projects/svn/javascript/trunk@830 1a71a410-84fe-4b95-a212-0ac519f4e53f

--- src/main/java/no/mesan/javascript/TestKontroller.java
+++ src/main/java/no/mesan/javascript/TestKontroller.java
@@ -1,6 +1,8 @@
 package no.mesan.javascript;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Klasse
@@ -8,5 +10,11 @@ import org.springframework.stereotype.Controller;
  * @author Christian Ihle
  */
 @Controller
+@RequestMapping("test")
 public class TestKontroller {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void test() {
+        System.out.println("test");
+    }
 }
