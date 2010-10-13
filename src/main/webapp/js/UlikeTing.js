698a18bc249048060725d56f449474a9678e0b25
Christian Ihle <christiani@mesan.no>
2010-10-13 11:00:34 +0000

    Tester namespacing av funksjoner
    git-svn-id: https://dev.mesan.no/projects/svn/javascript/trunk@861 1a71a410-84fe-4b95-a212-0ac519f4e53f

---
+++ src/main/webapp/js/UlikeTing.js
@@ -0,0 +1,12 @@
+
+// Best practice nr 1 - bruke namespace på alle funksjoner
+
+var Funksjoner = {};
+
+Funksjoner.pluss = function(tall1, tall2) {
+  return tall1 + tall2;
+}
+
+// Best practice nr 2 - ??
+
+// Best practice nr 3 - profit!
---
+++ src/test/webapp/js/UlikeTingTest.js
@@ -0,0 +1,5 @@
+UlikeTingTest = TestCase("UlikeTingTest");
+
+UlikeTingTest.prototype.testPluss = function() {
+  assertEquals(10, Funksjoner.pluss(2, 8));
+};
