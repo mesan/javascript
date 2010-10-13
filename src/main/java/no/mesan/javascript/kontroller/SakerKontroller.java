9c9972a7c011bb3ace2a7d88c24c9517e8e7d56d
Christian Ihle <christiani@mesan.no>
2010-10-13 11:41:52 +0000

    Legger saker i sesjonen
    git-svn-id: https://dev.mesan.no/projects/svn/javascript/trunk@864 1a71a410-84fe-4b95-a212-0ac519f4e53f

--- src/main/java/no/mesan/javascript/kontroller/SakerKontroller.java
+++ src/main/java/no/mesan/javascript/kontroller/SakerKontroller.java
@@ -3,11 +3,12 @@ package no.mesan.javascript.kontroller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import no.mesan.javascript.domene.Sak;
 import no.mesan.javascript.domene.SakStatus;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,8 +21,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("sak")
 public class SakerKontroller {
 
-    @ModelAttribute("saker")
-    public List<Sak> hentSaker() {
+    private static final String SAKER_ATTRIBUTT = "saker";
+
+    private List<Sak> lagOriginaleSaker() {
         final List<Sak> saker = new ArrayList<Sak>();
 
         saker.add(new Sak(1L, "Sak 1 tittel", "Sak 1 beskrivelse", SakStatus.NY));
@@ -32,8 +34,15 @@ public class SakerKontroller {
         return saker;
     }
 
+    private void leggSakerISesjonenHvisDeMangler(HttpSession sesjon) {
+        if (sesjon.getAttribute(SAKER_ATTRIBUTT) == null) {
+            sesjon.setAttribute(SAKER_ATTRIBUTT, lagOriginaleSaker());
+        }
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String visSide() {
+    public String visSide(HttpSession sesjon) {
+        leggSakerISesjonenHvisDeMangler(sesjon);
         return "saker";
     }
 }
