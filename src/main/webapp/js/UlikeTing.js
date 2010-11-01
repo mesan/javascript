4eb450b534e9af3591556bb715d1b6cbd9f5e6d5
Christian Ihle <christiani@mesan.no>
2010-11-01 13:27:49 +0000

    Enkel støtte for å opprette en ny sak
    git-svn-id: https://dev.mesan.no/projects/svn/javascript/trunk@922 1a71a410-84fe-4b95-a212-0ac519f4e53f

--- src/main/webapp/js/UlikeTing.js
+++ src/main/webapp/js/UlikeTing.js
@@ -12,6 +12,21 @@ Funksjoner.finnId = function(felt) {
   return id.match(/\d+/);
 }
 
+Funksjoner.opprettNyTomSak = function(tabell) {
+  jQuery(tabell).append(
+    "<tr id='rad_1'>" +
+      "<td id='id_1'>1</td>" +
+      "<td id='tittel_1'></td>" +
+      "<td id='beskrivelse_1'></td>" +
+      "<td id='sakStatus_1'>NY</td>" +
+      "<td id='operasjoner_1'>" +
+        "<input id='rediger_1' class='redigerKnapp' type='button' value='Rediger'/>&#160;&#160;" +
+        "<input id='slett_1' class='slettKnapp' type='button' value='Slett'/>" +
+      "</td>" +
+    "</tr>"
+  );
+}
+
 // Best practice nr 2 - ??
 
 // Best practice nr 3 - profit!
--- src/test/webapp/js/UlikeTingTest.js
+++ src/test/webapp/js/UlikeTingTest.js
@@ -12,5 +12,56 @@ TestCase("UlikeTingTest", {
   "test skal finne id 5 for slett knapp": function() {
     /*:DOC knapp = <input id="slett_5" type="button"/> */
     assertEquals(5, Funksjoner.finnId(this.knapp));
+  },
+
+  "test opprettNySak skal lage ny rad": function() {
+    /*:DOC += <table id="tabell"></table> */
+    assertEquals(0, jQuery("#tabell tr").size());
+
+    Funksjoner.opprettNyTomSak("#tabell");
+
+    assertEquals("Skal lage 1 ny rad", 1, jQuery("#tabell tr").size());
+    assertEquals("Skal lage rad med id", 1, jQuery("#tabell #rad_1").size());
+    assertEquals("Skal lage 5 kolonner", 5, jQuery("#tabell #rad_1 td").size());
+    assertEquals("Skal lage id kolonne", 1, jQuery("#tabell #rad_1 #id_1").size());
+    assertEquals("Skal lage tittel kolonne", 1, jQuery("#tabell #rad_1 #tittel_1").size());
+    assertEquals("Skal lage beskrivelse kolonne", 1, jQuery("#tabell #rad_1 #beskrivelse_1").size());
+    assertEquals("Skal lage sakStatus kolonne", 1, jQuery("#tabell #rad_1 #sakStatus_1").size());
+  },
+
+  "test opprettNySak skal fylle ut noen felter": function() {
+    /*:DOC += <table id="tabell"></table> */
+    assertEquals(0, jQuery("#tabell tr").size());
+
+    Funksjoner.opprettNyTomSak("#tabell");
+
+    assertEquals("1", jQuery("#tabell #rad_1 #id_1").text());
+    assertEquals("NY", jQuery("#tabell #rad_1 #sakStatus_1").text());
+  },
+
+  "test opprettNySak skal lage slett knapp i ny rad": function() {
+    /*:DOC += <table id="tabell"></table> */
+    assertEquals(0, jQuery("#tabell tr").size());
+
+    Funksjoner.opprettNyTomSak("#tabell");
+
+    var slett = jQuery("#tabell #rad_1 #operasjoner_1 #slett_1");
+    assertEquals("Skal lage knapp for slett i kolonne for operasjoner", 1, slett.size());
+    assertTrue("Slett skal ha riktig klasse", slett.hasClass("slettKnapp"));
+    assertEquals("Slett skal ha riktig type", "button", slett.attr("type"));
+    assertEquals("Slett skal ha riktig tekst", "Slett", slett.val());
+  },
+
+  "test opprettNySak skal lage rediger knapp i ny rad": function() {
+    /*:DOC += <table id="tabell"></table> */
+    assertEquals(0, jQuery("#tabell tr").size());
+
+    Funksjoner.opprettNyTomSak("#tabell");
+
+    var rediger = jQuery("#tabell #rad_1 #operasjoner_1 #rediger_1");
+    assertEquals("Skal lage knapp for rediger i kolonne for operasjoner", 1, rediger.size());
+    assertTrue("Rediger skal ha riktig klasse", rediger.hasClass("redigerKnapp"));
+    assertEquals("Rediger skal ha riktig type", "button", rediger.attr("type"));
+    assertEquals("Rediger skal ha riktig tekst", "Rediger", rediger.val());
   }
 });
