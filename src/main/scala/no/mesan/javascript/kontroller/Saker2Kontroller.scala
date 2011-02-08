25a51e290763deb40867325dbc77e56ffd9fec26
Christian Ihle <christiani@mesan.no>
2011-02-08 13:33:33 +0100

    JSP foreach liker ikke scala list, så konverterer til java list

--- src/main/scala/no/mesan/javascript/kontroller/Saker2Kontroller.scala
+++ src/main/scala/no/mesan/javascript/kontroller/Saker2Kontroller.scala
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod}
 import no.mesan.javascript.domene.{Sak, SakStatus}
 import javax.servlet.http.HttpSession
 import no.mesan.javascript.Ape
-import java.util.ArrayList
+import scala.collection.JavaConversions._
 
 @Controller
 @RequestMapping(Array("scalasak"))
 class Saker2Kontroller {
   private val SAKER_ATTRIBUTT: String = "saker"
 
-  private def lagOriginaleSaker: List[Sak] = List(
+  private def lagOriginaleSaker: java.util.List[Sak] = asJavaList(List(
     new Sak(1L, "Sak 1 tittel", "Sak 1 beskrivelse", SakStatus.NY),
     new Sak(2L, "Sak 2 tittel", "Sak 2 beskrivelse", SakStatus.IGANG),
     new Sak(3L, "Sak 3 tittel", "Sak 3 beskrivelse", SakStatus.NY),
-    new Sak(4L, "Sak 4 tittel", "Sak 4 beskrivelse", SakStatus.FERDIG))
+    new Sak(4L, "Sak 4 tittel", "Sak 4 beskrivelse", SakStatus.FERDIG)))
 
   private def leggSakerISesjonenHvisDeMangler(sesjon: HttpSession) = {
     if (sesjon.getAttribute(SAKER_ATTRIBUTT) == null) {
