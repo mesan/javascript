a1804848b27cb47598ac3d6ea81b89e71f22c702
Christian Ihle <christiani@mesan.no>
2011-03-02 16:04:24 +0100

    Lagt paa regex matching av java string

--- src/main/scala/no/mesan/javascript/domene/SakServiceImpl.scala
+++ src/main/scala/no/mesan/javascript/domene/SakServiceImpl.scala
@@ -18,6 +18,22 @@ class SakServiceImpl extends SakService {
   def oppdaterTittel(id: Long, tittel: String) = {
     val sak: Sak = hentSak(id)
     sak.setTittel(tittel)
+
+    val regex = "([0-9]*)([0-9]{2})".r
+    val r2 = "([a-z]{2})".r
+    sak.getTittel() match {
+      case regex(first, last) => println("Tall " + first + last)
+      case r2(tekst) => println("Tekst " + tekst)
+      case _ => println("Ukjent")
+    }
+
     sak
   }
 }
+
+abstract class SakCase
+case class Var(name: String) extends SakCase
+
+// binde scala objekt
+// bruke trait i kontrollere, og pattern matching, og case
+
